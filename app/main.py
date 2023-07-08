308bbd87d1b2ff87a2fd40c73a226be9d2f781ce
Kirill Netreba <knete-87@mail.ru>
2023-07-08 12:57:30 +0300

    Add CRUD for movies

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
-from fastapi import FastAPI
+import logging
+
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('app.log')
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 app = FastAPI()
 
@@ -23,3 +32,28 @@ class Movie(BaseModel):
 @app.get('/', response_model=list[Movie])
 async def index():
     return fake_movie_db
+
+
+@app.post('/movies', status_code=201)
+async def add_movie(payload: Movie):
+    logger.info(payload)
+    movie = payload.model_dump()
+    fake_movie_db.append(movie)
+    return {'id': len(fake_movie_db) - 1}
+
+
+@app.put('/movies/{movie_id}')
+def update_movie(movie_id: int, payload: Movie):
+    if 0 <= movie_id < len(fake_movie_db):
+        movie = payload.model_dump()
+        fake_movie_db[movie_id] = movie
+        return None
+    raise HTTPException(status_code=404, detail='Movie with given id not found')
+
+
+@app.delete('/movies/{movie_id}')
+def delete_movie(movie_id: int):
+    if 0 <= movie_id < len(fake_movie_db):
+        del fake_movie_db[movie_id]
+        return None
+    raise HTTPException(status_code=404, detail='Movie with given id not found')
